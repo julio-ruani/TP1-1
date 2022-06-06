1b3a30be515555633b5b4dd718232076f88c21ab
julio-ruani <julio.ruani.ispc@gmail.com>
2022-06-06 19:57:42 -0300

    3er commir resta

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ print("Trabajo Practico N.1 - Ejercicio 1")
 def suma (a,b):
     return (a+b)
 
+def resta(a,b):
+    return (a-b)
+
 if __name__ == "__main__":
     print ("Primer programa en Python")
     print("La suma es: ", str(suma(2,3)))
