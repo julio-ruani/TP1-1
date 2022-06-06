bf29cf5f26ebe25e344861a33a1b38e95785db89
julio-ruani <julio.ruani.ispc@gmail.com>
2022-06-06 20:27:11 -0300

    resta en modulo aparte

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
+from resta import resta
 print("Trabajo Practico N.1 - Ejercicio 1")
 
 def suma (a,b):
     return (a+b)
 
-def resta(a,b):
-    return (a-b)
-
 if __name__ == "__main__":
     print ("Primer programa en Python")
     print("La suma es: ", str(suma(2,3)))
+    print("La resta es: ", str(resta(2,3)))
