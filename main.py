c3b95e4516968cb747779d3066949cb1a158b123
julio-ruani <julio.ruani.ispc@gmail.com>
2022-06-06 19:53:35 -0300

    suma

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 print("Trabajo Practico N.1 - Ejercicio 1")
+
+def suma (a,b):
+    return (a+b)
+
+if __name__ == "__main__":
+    print ("Primer programa en Python")
+    print("La suma es: ", str(suma(2,3)))
